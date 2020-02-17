29e42c1c6c86fb4452944e648436f467f40ba609
Jacob Yoon <jacobyoon926@gmail.com>
2020-02-17 12:03:59 -0500

    Update

--- js/script.js
+++ js/script.js
@@ -4,12 +4,12 @@ window.onscroll = function() {
 
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 100) {
-        document.getElementById("header").style.background = "white";
+        //document.getElementById("header").style.background = "white";
         document.getElementById("header").style.height = "80px";
         $('.logo').css('padding', '20px');
         $('.links').css('padding', '31px 40px');
     } else {
-        document.getElementById("header").style.background = "none";
+        //document.getElementById("header").style.background = "none";
         document.getElementById("header").style.height = "120px";
         $('.logo').css('padding', '40px');
         $('.links').css('padding', '51px');
